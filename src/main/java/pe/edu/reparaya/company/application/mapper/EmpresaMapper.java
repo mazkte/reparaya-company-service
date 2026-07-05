a83a35149dc85f925beba42b2a91b910e0b61662
Mazkte <mazcarate@proveedor.intercorp.com.pe>
2026-07-04 23:33:45 -0500

    fix: update empresaid

--- src/main/java/pe/edu/reparaya/company/application/mapper/EmpresaMapper.java
+++ src/main/java/pe/edu/reparaya/company/application/mapper/EmpresaMapper.java
@@ -14,12 +14,12 @@ public interface EmpresaMapper {
  @Mapping(target = "porcentajeCarga", expression = "java(empresa.porcentajeCarga())")
  @Mapping(target = "contratoVigente", expression = "java(empresa.contratoVigente())")
  @Mapping(target = "contratoVenceProximamente", expression = "java(empresa.contratoVenceProximamente(30))")
- @Mapping(target = "id", source = "id")
  EmpresaResponse toResponse(EmpresaServicio empresa);
 
  List<EmpresaResponse> toResponseList(List<EmpresaServicio> empresas);
 
  @Mapping(target = "porcentajeCarga", expression = "java(empresa.porcentajeCarga())")
+ @Mapping(target = "empresaId", source = "id")
  DisponibilidadResponse toDisponibilidadResponse(EmpresaServicio empresa);
 
  List<DisponibilidadResponse> toDisponibilidadList(List<EmpresaServicio> empresas);
